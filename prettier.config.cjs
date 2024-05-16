b14b26fac4aac6770af4ba7c8a67d55a466b0769
mouro <dolbrebr@gmail.com>
2024-05-16 12:04:46 -0300

    fix: atualizado configuracoes e cores do site

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,19 +1,27 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  // i am just using the standard config, change if you need something else
-  
-  ...require('prettier-config-standard'),
-  pluginSearchDirs: [__dirname],
-  plugins: [require.resolve('prettier-plugin-astro'), require.resolve('prettier-plugin-tailwindcss')],
-  overrides: [
-    {
-      files: ['*.astro' , '*.json'],
-      options: {
-        parser: [
-          'astro',
-          'json'
-        ]
-      }
-    }
-  ]
+    
+
+    ...require('prettier-config-standard'),
+    pluginSearchDirs: [__dirname],
+    plugins: [
+        require.resolve('prettier-plugin-astro'),
+        require.resolve('prettier-plugin-tailwindcss')
+    ],
+    overrides: [
+        {
+            files: ['*.astro', '*.json', '*.js'],
+            options: {
+                parser: ['astro', 'json']
+            }
+        }
+    ],
+
+    "useTabs": true,
+    "singleQuote": true,
+    "trailingComma": "none",
+    "semi": false,
+    "printWidth": 100
+
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,40 +9,22 @@ export default {
       },
 
 			colors: {
-				dark: {
-          background: 'hsl(225, 9%, 9%)',
-          foreground: 'hsl(210, 40%, 98%)',
-          card: 'hsl(228, 9%, 11%)',
-          'card-foreground': 'hsl(210, 40%, 98%)',
-          popover: 'hsl(228, 9%, 11%)',
-          'popover-foreground': 'hsl(210, 40%, 98%)',
-          primary: 'hsl(252, 100%, 69%)',
-          'primary-foreground': 'hsl(0, 0%, 100%)',
-          secondary: 'hsl(228, 6%, 16%)',
-          'secondary-foreground': 'hsl(210, 40%, 98%)',
-          muted: 'hsl(228, 6%, 16%)',
-          'muted-foreground': 'hsl(215, 20.2%, 65.1%)',
-          accent: 'hsl(228, 6%, 16%)',
-          'accent-foreground': 'hsl(210, 40%, 98%)',
-          destructive: 'hsl(0, 84%, 60%)',
-          'destructive-foreground': 'hsl(210, 40%, 98%)',
-          border: 'hsl(228, 6%, 16%)',
-          input: 'hsl(228, 6%, 16%)',
-          ring: 'hsl(212.7, 26.8%, 83.9%)',
-          radius: '0.5rem',
-        },
+				heading: '#fcfcfc',
+        bodyLight: '#b5b7bf',
+        bodyDark: '#93959c',
+        overlay: '#30323b',
+        border: '#33363d',
+        bg: '#1D1E24',
+        surface: '#23262b',
+        accent: '#0090FF',
+       
+
 
 				
 
 			},
 
-			gridTemplateRows: {
-				newRow: 'auto 1fr auto',
-			},
-
-			container: {
-				center: true,
-			},
+			
 
 		},
 	},
