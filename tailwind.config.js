f658f8150f21cc9a81a31a0c0cab5f2bb3f4368b
mouro <dolbrebr@gmail.com>
2024-05-18 13:17:53 -0300

    footer atualizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,}'],
 	theme: {
     extend: {
-      fontFamily: {
-        sans: ['"IBM Plex Sans"',  'sans-serif'],
-        mono: ['"Monaspace Neon"', 'monospace'],
-      },
-
+     
 			colors: {
 				heading: '#fcfcfc',
         bodyLight: '#b5b7bf',
@@ -17,10 +13,7 @@ export default {
         bg: '#1D1E24',
         surface: '#23262b',
         accent: '#0090FF',
-       
-
-
-				
+      
 
 			},
 
